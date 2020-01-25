a98a918988c27b5cf7fad6dad93a01dc9610d588
Bala Raman <srbala@gmail.com>
2020-01-24 21:15:49 -0500

    Added docs

---
+++ docs/precache-manifest.2fc2a29726517f6b7b6b8505ab41e849.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f89c65e458fc1ec5704ba016fd6317",
+    "url": "/sigma-react/index.html"
+  },
+  {
+    "revision": "c6d7e4ad47b5b1bf7a24",
+    "url": "/sigma-react/static/css/2.bee801a2.chunk.css"
+  },
+  {
+    "revision": "1fa02235b2417ac55b1b",
+    "url": "/sigma-react/static/css/main.bc6e4fb4.chunk.css"
+  },
+  {
+    "revision": "c6d7e4ad47b5b1bf7a24",
+    "url": "/sigma-react/static/js/2.12cd62a2.chunk.js"
+  },
+  {
+    "revision": "daecc8598b982650c08096ac9e7caf5f",
+    "url": "/sigma-react/static/js/2.12cd62a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fa02235b2417ac55b1b",
+    "url": "/sigma-react/static/js/main.570e88a5.chunk.js"
+  },
+  {
+    "revision": "49967c14fdb0ae78868a",
+    "url": "/sigma-react/static/js/runtime-main.bf57be22.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/sigma-react/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/sigma-react/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/sigma-react/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "2d2afb2719a1ee903e576e7c457daf81",
+    "url": "/sigma-react/static/media/primeicons.2d2afb27.eot"
+  },
+  {
+    "revision": "66ee0deb739ca71f0ecdc39d7c1b22cb",
+    "url": "/sigma-react/static/media/primeicons.66ee0deb.woff"
+  },
+  {
+    "revision": "df0140f8e79ecfeffaf85220aaecd7c4",
+    "url": "/sigma-react/static/media/primeicons.df0140f8.ttf"
+  },
+  {
+    "revision": "e5e0e94474d5fd92e7e800a8865d297c",
+    "url": "/sigma-react/static/media/primeicons.e5e0e944.svg"
+  }
+]);
